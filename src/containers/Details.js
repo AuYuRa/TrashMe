22413a11981e89f233e3261416c9d959f2f867de
Yu Wati Nyi <yuwatinyi@nat-wireless-eduroam-guest-153-26.bu.edu>
2022-10-30 12:24:45 -0400

    final design

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -15,9 +15,15 @@ const Details = () => {
           return(
             <div>
               <h2> {tag} </h2>
-              <p className='text'> {TAGS[tag].trash_bin} </p>
+              <div className = 'card'>
+              <p className='trashBinstitle'> {TAGS[tag].trash_bin} </p>
               <img alt = 'items' src = {`${TAGS[tag].bin_image}`} style = {{width:200 , height:200}} />
               <p className='text'> {TAGS[tag].fact} </p>
+              </div>
+              <div className="circle1"></div>
+              <div className="circle2"></div>
+              <div className="circle3"></div>
+              <div className="circle4"></div>
             </div>);
       })}
     </div>
